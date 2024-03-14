c6a8cb1e84ad146d5fa94b20267adeb4c79516c2
dreavr <1o.vazquez.ramirez.andrea@gmail.com>
2024-03-14 10:31:01 -0700

    header no responsive

--- games-finder/src/app/page.jsx
+++ games-finder/src/app/page.jsx
@@ -3,7 +3,22 @@ import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-1 max-h-max p-24">
+    
+    <main className=" max-h-max items-center justify-center mx-auto p-4">
+
+    <header className="flex mt-0 mb-4 justify-center items-center bg bg-gray-950 p-6 text-2xl font-semibold">
+    <div className="flex items-center gap-x-2">
+        <Image
+            src= "/images/HeadMashUpW.png"
+            width={40}
+            height={40}
+            alt="logoHeader"
+            className=""
+          />
+          <h1 className="md">Mash-Up</h1>
+    </div>
+    </header>
+
       <div className="flex justify-evenly space-x-4 grid-flow-col w-full h-full">
         <div className="w-1/2 text-left text-7xl">Universe of games, at the tap of your finger
         </div>
